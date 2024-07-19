ddb72a72264de8bf3f013d396a8aa2aaf8aab722
Reboot9 <sergejskorikjr@gmail.com>
2024-07-19 16:05:34 +0300

    feat: add profanity check when saving post model

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -2,6 +2,8 @@ from ckeditor.fields import RichTextField
 from django.contrib.auth import get_user_model
 from django.db import models
 
+from profanity_check import predict_prob
+
 User = get_user_model()
 
 
@@ -18,3 +20,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        """
+        Check for profanity in title and content
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        if predict_prob([self.title])[0] > 0.5 or predict_prob([self.content])[0] > 0.5:
+            self.is_blocked = True
+        super().save(*args, **kwargs)
